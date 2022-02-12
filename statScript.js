d69c7ac228abf3b16d98f76b21aba18264e6e3f1
seandhammond99 <seandhammond99@gmail.com>
2022-02-12 16:09:30 +0000

    bugfixing

--- statScript.js
+++ statScript.js
@@ -1,14 +1,3 @@
-function AllRounder(level, stage) {
-  this.level = level;
-  this.stage = stage;
-  this.health = 3;
-  this.attack = 3;
-  this.defence = 2;
-  this.spattack = 3;
-  this.spdefence = 2;
-  this.speed = 2;
-}
-
 function myFunction() {
     var build = document.getElementById("build");
     document.getElementById("health").innerHTML = "Health: " + 5;
