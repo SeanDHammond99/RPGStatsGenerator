c0f8119e8e755dd01770287f31ba4a0a7c9dae79
seandhammond99 <seandhammond99@gmail.com>
2022-02-12 15:08:24 +0000

    initial commit- index, started adding functionality

---
+++ allrounder.js
@@ -0,0 +1,12 @@
+function AllRounder(level, stage) {
+  this.level = level;
+  this.stage = stage;
+  this.health = 3;
+  this.attack = 3;
+  this.defence = 2;
+  this.spattack = 3;
+  this.spdefence = 2;
+  this.speed = 2;
+}
+
+module.exports = AllRounder
---
+++ statScript.js
@@ -0,0 +1,5 @@
+
+function myFunction() {
+    import AllRounder from 'allrounder.js'
+    document.getElementById("statblock").innerHTML="Paragraph changed.";
+}
