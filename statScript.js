b147ad28b44db3ae7b67d48ad34e6c9548ea0e27
seandhammond99 <seandhammond99@gmail.com>
2022-02-12 21:02:36 +0000

    testing getting values

--- statScript.js
+++ statScript.js
@@ -1,7 +1,11 @@
 import AllRounder from './allrounder.js'
 
 function update() {
-    var build = document.getElementById("build");
-    document.getElementById("health").innerHTML = build;
+    var build = document.getElementById("build").value;
+    var stage = document.getElementById("stage").value;
+    var level = document.getElementById("level").value;
+
+
+
 }
 document.getElementById("button").addEventListener("click", update);
